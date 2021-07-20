48471555c4fc4f6672cda16de98f2e3c43e30547
Bruno Morais <mizerablebr@gmail.com>
2021-07-20 11:53:26 -0300

    Extract SearchBox class

---
+++ searchbox.js
@@ -0,0 +1,144 @@
+class SearchBox {
+    constructor(keys, containerId) {
+        this.keys = keys;
+        this.containerId = containerId;
+        this.searchMenu = {};
+        this.searchBox = {};
+        this.searchInputBox = {};
+        this.searchInput = {};
+        this.searchBoxContainerTemplate = '<div class="search-box border m-3 p-2 d-flex bg-white"><div class="search-input-box d-flex flex-grow-1" data-toggle="dropdown"><input class="search-input flex-grow-1 text-uppercase" data-value-mode="false" type="text" style="border: 0; outline:0;"/></div><div class="search-menu dropdown-menu mt-3" aria-labelledby="dropdownMenuButton"></div></div>'
+        this.keyItem = '<div class="search-item search-key px-1 mr-1 text-uppercase" data-key="{}" style="color: #707070; background-color: #dbdbdb; padding-top: .20rem;"><span>{}</span>:</div>'
+        this.valueItem = '<div class="search-item search-value px-1 mr-2 text-uppercase" style="background-color: #dbdbdb; padding-top: .20rem;">{}</div>'
+        this.menuItem = '<a class="menu-search-key dropdown-item" data-key="{}" href="#">{}</a>';
+        this.containerId = 'searchBoxContainer';
+
+        this.initialSetup();
+
+        this.addListenerToSearchKeys();
+        this.addListenerToSearchInput();
+    }
+
+    static create(keys, containerId) {
+        if (!Array.isArray(keys))
+            throw "Use Array of String as Keys";
+        return new SearchBox(keys, containerId);
+    }
+
+    formatTemplate(term, template) {
+        return template.replace(/\{\}/g, term);
+    }
+    
+
+    initialSetup() {
+        $('#' + this.containerId).html(this.searchBoxContainerTemplate);
+        this.searchMenu = $('.search-menu');
+        this.searchBox = $('.search-box');
+        this.searchInputBox = $('.search-input-box');
+        this.searchInput = $('.search-input');
+
+        this.keys.forEach(function(item) {
+            this.searchMenu.append(this.formatTemplate(item, this.menuItem));
+        }, this);
+    }
+
+    addListenerToSearchKeys() {
+        const menuSearchKeys = $('.menu-search-key');
+        menuSearchKeys.off();
+        menuSearchKeys.on('click', this, function(event){
+            event.data.addKeyToSearchBox(event.target.dataset.key);
+            event.data.searchInput.focus();
+        });
+    }
+
+    addListenerToSearchInput() {
+        this.searchInput.on('keydown', this, this.handleInputKeyPress);
+        this.searchInput.on('keyup', this, this.filterSearchMenuUsingInputValue);
+    }
+
+    addKeyToSearchBox(key) {
+        this.searchInput.val('');
+        this.setValueMode();
+        this.searchInputBox.before(this.formatTemplate(key, this.keyItem))
+        this.updateSearchMenu();
+    }
+
+    updateSearchMenu() {
+        let searchKeys = $('.search-key');
+        if (searchKeys.length > 0) {
+            let usedKeys = searchKeys.map( (index, searchKey) => {
+                return searchKey.dataset.key;
+            }).toArray();
+            let keysToDisplay = this.keys.filter(key => !usedKeys.includes(key));
+            this.searchMenu.children().remove();
+            keysToDisplay.forEach(function(item) {
+                this.searchMenu.append(this.formatTemplate(item, this.menuItem));
+                this.addListenerToSearchKeys();
+            }, this);
+        }
+    }
+
+    setValueMode() {
+        this.searchInput.data('value-mode', true);
+        this.searchInputBox.attr('data-toggle', '');
+        this.searchMenu.dropdown('update')
+        this.searchMenu.hide();
+    }
+
+    setKeyMode() {
+        this.searchInput.data('value-mode', false);
+        this.searchInputBox.attr('data-toggle', 'dropdown');
+        this.searchInputBox.data('toggle', 'dropdown');
+        this.searchMenu.dropdown('update')
+        this.searchMenu.show();
+    }
+
+    handleInputKeyPress(event) {
+        let inputValue = event.target.value;
+        let key = event.key;
+        if (inputValue.length === 0 && key === 'Backspace')
+            event.data.removeLastKeyFromSearchBox();
+        
+        if (event.data.searchInput.data('value-mode')) {
+
+            if (key === ' ' || key == 'Enter') {
+                event.data.createSearchValueFromInput();
+                event.data.searchInput.val('');
+            }
+        }
+    }
+
+    removeLastKeyFromSearchBox() {
+        let searchItens = $('.search-item');
+        if (searchItens.length > 0) {
+            let lastKey = searchItens.last();
+            if (lastKey.hasClass('search-key'))
+                this.setKeyMode();
+            else
+                this.setValueMode();
+            
+            searchItens.last().remove();
+            this.updateSearchMenu();
+        }
+    }
+
+    createSearchValueFromInput() {
+        let inputValue = this.searchInput.val();
+        this.searchInputBox.before(this.formatTemplate(inputValue, this.valueItem));
+        this.searchInput.val('');
+        this.setKeyMode();
+    }
+
+    filterSearchMenuUsingInputValue(event) {
+        if (!event.data.searchInput.data('value-mode')) {
+            let inputValue = event.target.value.trim();
+            let keysThatDontMatchInput = event.data.keys.filter(value => !value.toUpperCase().includes(inputValue.toUpperCase()));
+            $('.menu-search-key').each((index, menuSearchKey) => {
+                if (keysThatDontMatchInput.includes(menuSearchKey.dataset.key))
+                    $(menuSearchKey).hide();
+                else
+                    $(menuSearchKey).show();
+            })
+        }
+    }
+
+}
